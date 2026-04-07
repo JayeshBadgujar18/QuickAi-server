373d9873e4f9f51a96c6df3dff9f951a424b7d3e
Jayesh Badgujar <badgujarjayesh491@gmail.com>
2026-04-08 01:32:08 +0530

    feat: initialize server with database configuration, environment support, and basic express setup

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('QuickAi Server is running...');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
